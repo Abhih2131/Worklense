1d9fb7204aae56558a1b9a14c305a9d50f321e7e
Abhishek Haldankar <abhishek.haldankar83@gmail.com>
2025-05-16 20:09:07 +0530

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -2,6 +2,7 @@
 import pandas as pd
 
 def load_data():
+    # Ensure correct paths for data files
     employee_df = pd.read_excel('data/employee_master.xlsx')
     leave_df = pd.read_excel('data/HRMS_Leave.xlsx')
     sales_df = pd.read_excel('data/Sales_INR.xlsx')
